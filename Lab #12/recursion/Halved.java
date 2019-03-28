4211352a88b87b04628b9f94f0b1c7cc18a7b392
jpvp13 <33247490+jpvp13@users.noreply.github.com>
2019-03-27 18:02:56 -0700

    Add files via upload

---
+++ Lab #12/recursion/Halved.java
@@ -0,0 +1,20 @@
+package recursion;
+
+public class Halved {
+
+	// Return the result of dividing n by 2, ignoring any 
+	// fractional parts of the result. For example, halved(6) 
+	// and halved(7) should both return 3. Assume n will not 
+	// be negative.
+	public int halved(int n) {
+		// Fill-in the test for the conditional below,
+		// but don't change anything else.
+		if(  ) {
+			return 0;
+		} else {
+			return (1 + halved(n - 2));
+		}
+	}
+
+	
+}
